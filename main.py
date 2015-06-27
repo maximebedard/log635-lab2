2121610705372429efdf9b3ac8af3c04521d91e0
Maxime Bedard <maxime.bedard@shopify.com>
2015-06-26 22:12:16 -0400

    Revert "Revert "Added warning in python""
    This reverts commit aa1852c414ed70eb9915728c85814f0392204f1b.
    
    Conflicts:
    	grammaire.cfg
    	main.py
    	texte.txt

--- main.py
+++ main.py
@@ -13,34 +13,82 @@ class RulesGenerator:
     self.loadGrammar()
     self.loadSentences()
 
+  """
+  Load grammar file and initialize the parser
+  """
   def loadGrammar(self):
     with open(self.grammarFile, 'r') as f:
       my_grammar = grammar.FeatureGrammar.fromstring(f.read())
       self.parser = nltk.FeatureEarleyChartParser(my_grammar)
 
+  """
+  Load all the sentences into memory
+  """
   def loadSentences(self):
     with open(self.sentencesFile, 'r') as f:
       self.sentences = f.readlines()
 
+  """
+  Generate an output file for all the rules
+  """
   def generateRules(self, outputFile):
     with open(outputFile, 'w') as f:
       for sentence in self.sentences:
-        if sentence.startswith('#'): continue
 
         sanitizedSentence = self.sanitizeSentence(sentence)
-        sys.stdout.write(sanitizedSentence)
 
-        trees = self.parser.parse(sanitizedSentence.split())
-        for tree in trees:
-          print(tree)
-          print(tree.label()['SEM'])
-          tree.draw()
+        if sentence.startswith('#'):
+          print('Skipping <{0}>'.format(sanitizedSentence))
+          continue
 
+        print('Parsing <{0}>'.format(sanitizedSentence))
+
+        trees = self.parse(sanitizedSentence)
+
+        self.showAmbiguousWarning(trees)
+        self.showTrees(trees)
+
+  def showTrees(self, trees):
+    for tree in trees:
+      label = tree.label()['SEM']
+      print(label)
+
+      self.showHardToParseWarning(label)
+      print(tree)
+      tree.draw()
+
+  """
+  Display a warning if symbols are present in the label
+  """
+  def showHardToParseWarning(self, label):
+    if re.search(r'[\\\.]', str(label)):
+      print(' => this sentence will be hard to parse')
+
+  """
+  Display a warning if the grammar is ambiguous
+  """
+  def showAmbiguousWarning(self, trees):
+    if len(trees) > 1:
+      print(' => is ambiguous...')
+
+  """
+  Return a list of tokens from sentence
+  """
   def sanitizeSentence(self, sentence):
-    sanitizedSentence = re.sub(r'(\.|\,|\')', ' ', sentence).lower()
+    sanitizedSentence = re.sub(r'(\.|\,|\'|\#)', ' ', sentence).strip('\r\n').strip().lower()
     sanitizedSentence = ''.join((c for c in unicodedata.normalize('NFD', sanitizedSentence) if unicodedata.category(c) != 'Mn'))
     return sanitizedSentence
 
+  """
+  Returns a list of trees generated from the sentence
+  """
+  def parse(self, sentence):
+    tokens = sentence.split()
+    return list(self.parser.parse(tokens))
+
+  """
+  Generate the jess rule from the generated sentence label
+  """
   def writeRule(self, f, tree):
     #sem = tree.label()['SEM']
     pass
